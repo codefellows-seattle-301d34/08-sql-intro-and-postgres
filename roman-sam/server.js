c316f0c0d165d7f951f8970d982e4f1ce5c0bd11
Sam Porter Giffords <sampgiffords@Sams-MBP.localdomain>
2018-05-17 12:19:58 -0700

    All SQL commands in js file now work

--- roman-sam/server.js
+++ roman-sam/server.js
@@ -35,11 +35,13 @@ app.get('/new-article', (request, response) => {
 });
 
 
-// REVIEW: Routes for making API calls to use CRUD Operations on our database
+// REVIEWED: Routes for making API calls to use CRUD Operations on our database
 app.get('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   //#3 query
-  //
+  // fetchAll 
+  // CRUD - get
+
   client.query('SELECT * FROM articles;')
     .then(function(result) {
       response.send(result.rows);
@@ -51,7 +53,9 @@ app.get('/articles', (request, response) => {
 
 app.post('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // #4 -
+  // 
+  // CRUD = PUT
   let SQL = `
     INSERT INTO articles(title, author, "authorUrl", category, "publishedOn", body)
     VALUES ($1, $2, $3, $4, $5, $6);
@@ -67,8 +71,9 @@ app.post('/articles', (request, response) => {
   ]
 
   client.query( SQL, values )
-    .then(function() {
-      response.send('insert complete')
+    .then(function(data) {
+      // console.log('LINE 75 s/ays this -- insert complete', data)
+      response.send('LINE 75 says this -- insert complete')
     })
     .catch(function(err) {
       console.error(err);
@@ -79,8 +84,15 @@ app.put('/articles/:id', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // PUT YOUR RESPONSE HERE
 
-  let SQL = '';
-  let values = [];
+  let SQL = 'UPDATE articles SET author = $1, "authorUrl" = $2, body = $3, category = $4, "publishedOn" = $5, title = $6 WHERE article_id = $7' ;
+  let values = [request.body.author, 
+                request.body.authorUrl,
+                request.body.body,
+                request.body.category,
+                request.body.publishedOn,
+                request.body.title,
+              request.params.id];
+              console.log('Do you see req, body, auth ? : ' + request.body.author);
 
   client.query( SQL, values )
     .then(() => {
@@ -111,7 +123,7 @@ app.delete('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // PUT YOUR RESPONSE HERE
 
-  let SQL = '';
+  let SQL = 'DELETE FROM articles';
   client.query( SQL )
     .then(() => {
       response.send('Delete complete')
